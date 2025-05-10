0cb93851ee8f64363d4a51c794ab1b1e86f3b2d1
Elvin <elvinhaxverdiyev777@gamil.com>
2025-05-10 23:01:06 +0400

    added name filter for products

--- promart/services/products_services/products/documents.py
+++ promart/services/products_services/products/documents.py
@@ -25,7 +25,8 @@ class ProductDocument(Document):
         ]
 
 
-def get_filtered_products(min_price=None, max_price=None):
+def get_filtered_products(min_price=None, max_price=None, product_name=None):
+    
     search = ProductDocument.search()
     if min_price is not None and max_price is not None:
         search = search.filter('range', price={'gte': min_price, 'lte': max_price})
@@ -33,6 +34,11 @@ def get_filtered_products(min_price=None, max_price=None):
         search = search.filter('range', price={'gte': min_price})
     elif max_price is not None:
         search = search.filter('range', price={'lte': max_price})
+        
+    # Product name filtering
+    if product_name:
+        search = search.query('match', name={'query': product_name, 'operator': 'and'})
+
     
     results = search.execute()
     products = [
--- promart/services/products_services/products/views/filter_products.py
+++ promart/services/products_services/products/views/filter_products.py
@@ -8,11 +8,14 @@ class ProductFilterAPIView(APIView):
     def get(self, request):
         min_price = request.query_params.get('min_price', None)
         max_price = request.query_params.get('max_price', None)
+        product_name = request.query_params.get('product_name', None)
         
         try:
             min_price = float(min_price) if min_price else None
             max_price = float(max_price) if max_price else None
-            products = get_filtered_products(min_price, max_price)
+            
+            products = get_filtered_products(min_price, max_price, product_name)
+            
             return Response({'products': products}, status=status.HTTP_200_OK)
         except ValueError:
             return Response({'error': 'Invalid price format'}, status=status.HTTP_400_BAD_REQUEST)
